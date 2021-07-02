8204c738b2e50ae7185a439a409712f956a9ec39
Sylvain Gougouzian <sylvain@lidrea.fr>
2021-07-02 09:09:55 +0200

    [+] promises

--- src/js/app.js
+++ src/js/app.js
@@ -34,6 +34,7 @@ const Gp = require('geoportal-access-lib/dist/GpServices');
 
 let layers = [];
 let places = [];
+let address_count = 1;
 
 document.querySelector('#calc').addEventListener('click', () => {
     layers.forEach((layer) => {
@@ -42,52 +43,77 @@ document.querySelector('#calc').addEventListener('click', () => {
 
     places = [];
 
-    document.querySelectorAll("#address input").forEach((input) => {
-        geocoder
-            .search({ q: input.value })
-            .then((response) => {
-                let place = [response[0].lon, response[0].lat];
-                places.push(place);
-                let point = new Point(place);
-                const layer = new VectorLayer({
-                    source: new VectorSource({
-                        features: [new Feature(point)],
-                    }),
-                    style: new Style({
-                        image: new Circle({
-                            radius: 3,
-                            fill: new Fill({ color: "red" }),
+    let getPlace = (address) => {
+        return new Promise((resolve) => {
+            geocoder
+                .search({ q: address })
+                .then((response) => {
+                    let place = [response[0].lon, response[0].lat];
+                    places.push(place);
+                    let point = new Point(place);
+                    const layer = new VectorLayer({
+                        source: new VectorSource({
+                            features: [new Feature(point)],
                         }),
-                    }),
+                        style: new Style({
+                            image: new Circle({
+                                radius: 3,
+                                fill: new Fill({ color: "red" }),
+                            }),
+                        }),
+                    });
+                    layers.push(layer);
+                    map.addLayer(layer);
+                    resolve();
+                })
+                .catch((error) => {
+                    console.log(error);
                 });
-                layers.push(layer);
-                map.addLayer(layer);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        })
+    }
+
+    let address = [];
+
+    document.querySelectorAll("#address input").forEach((input) => {
+        if (input.value.trim() != "")
+            address.push(input.value);
     });
 
+    let actions = address.map(getPlace);
 
-    setTimeout(() => {
-        // a changer avec les promises
-        Gp.Services.route({
-            apiKey: "jhyvi0fgmnuxvfv0zjzorvdn",
-            startPoint: { x: places[0][0], y: places[0][1] },
-            endPoint: { x: places[1][0], y: places[1][1] },
-            graph: "Voiture",
-            distanceUnit: "m",
-            onSuccess: function (result) {
-                console.log(result)
-            }
+    Promise.all(actions)
+        .then(() => {
+            if (places.length > 1)
+                Gp.Services.route({
+                    apiKey: "jhyvi0fgmnuxvfv0zjzorvdn", // api key found on npmjs
+                    startPoint: { x: places[0][0], y: places[0][1] },
+                    endPoint: { x: places[1][0], y: places[1][1] },
+                    graph: "Voiture",
+                    distanceUnit: "m",
+                    onSuccess: function (result) {
+                        const distance = result.totalDistance;
+                        console.log(distance, result)
+                    }
+                });
         });
-    }, 5000);
 
 }, true)
 
 window.newAddress = () => {
-    const html = document.querySelector('#address .wrapper .flex').innerHTML;
-    document.querySelector('#address .wrapper').innerHTML += '<div class="flex">' + html + '</div>';
+    const div = document.createElement("div");
+    div.classList.add('flex');
+    div.innerHTML = `
+        <input id="address_${address_count++}"type="text" placeholder="Nouvelle adresse" />
+        <button class="minus" onclick="window.removeAddress(this)">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 12H18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> 
+            </svg>
+        <button onclick="window.newAddress()">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 12H12M18 12H12M12 12V6M12 12V18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+    `;
+    document.querySelector('#address .wrapper').appendChild(div);
 }
 
 window.removeAddress = (element) => {
